d4ea352136c3d177bddcca79d144e2cfae9801c8
Milan Kolar <milan@pype.club>
2018-11-28 16:29:36 +0100

    addng tray debug

--- launchers/traydebug.bat
+++ launchers/traydebug.bat
@@ -7,7 +7,7 @@ pushd %~dp0..\..\..
 set PYPE_SETUP_ROOT=%cd%
 
 :: debugging
-set PYPE_DEBUG=1
+set PYPE_DEBUG=0
 set PYPE_DEBUG_STDOUT=1
 
 :: maintain python environment
@@ -18,4 +18,4 @@ set REMOTE_ENV_ON=0
 call %PYPE_SETUP_ROOT%\bin\launch_conda.bat
 
 ::python %PYPE_SETUP_ROOT%\app\cli.py %*
-pype --tray
+pype --traydebug
