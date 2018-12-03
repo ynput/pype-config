36acfee1124c7d1f8ee31d6f631e803ee2bd9bd2
Jakub Jezek <jezschaj@gmail.com>
2018-12-03 12:34:18 +0100

    adding ocio, fix nuke11.2 laucher, changing pype-config to accept ocio and software environments

--- bin/windows/nuke11.2.bat
+++ bin/windows/nuke11.2.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set __app__="Nuke11.2v5"
-set __exe__="C:\Program Files\Nuke11.2v5\Nuke11.2.exe"
+set __app__="Nuke11.2v2"
+set __exe__="C:\Program Files\Nuke11.2v2\Nuke11.2.exe"
 if not exist %__exe__% goto :missing_app
 
 start %__app__% %__exe__% %*
