33c7945f76293308ca6f006e24f44feb243901fd
Jakub Trllo <jakub.trllo@gmail.com>
2018-12-06 11:46:36 +0100

    added launchers for event server

---
+++ launchers/ftrack_event_server.bat
@@ -0,0 +1,23 @@
+@echo off
+taskkill /FI "WINDOWTITLE eq Pype's*" /F
+
+title Pype's tray app
+
+:: set basic environments
+pushd %~dp0..
+set PYPE_STUDIO_TEMPLATES=%cd%
+pushd %~dp0..\..\..
+set PYPE_SETUP_ROOT=%cd%
+
+:: debugging
+set PYPE_DEBUG=3
+set DEBUG=%PYPE_DEBUG%
+:: maintain python environment
+:: will synchronize remote with local
+set SYNC_ENV=0
+:: will switch to remote
+set REMOTE_ENV_ON=0
+call %PYPE_SETUP_ROOT%\bin\launch_conda.bat
+
+::python %PYPE_SETUP_ROOT%\app\cli.py %*
+start "Pype's event server" pype --eventserver
---
+++ launchers/ftrack_event_server.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Pype-setup
+# Linux client launcher
+#
+
+# Full path of the current script
+THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
+# The directory where current script resides
+DIR=`dirname "${THIS}"`
+
+export PYPE_STUDIO_TEMPLATES="$(cd $DIR/../ ; pwd)"
+export PYPE_SETUP_ROOT="$(cd $DIR/../../../ ; pwd)"
+# Directory, where will be local evironment. Should be accessible for all
+CONDA_SHARED="/tmp"
+
+# debugging
+export PYPE_DEBUG=3
+export PYPE_DEBUG_STDOUT=0
+
+# maintain python environment
+SYNC_ENV=0 # will synchronize remote with local
+REMOTE_ENV_ON=0 # will switch to remote
+
+# Load colors definitions for easy output coloring
+source "$PYPE_SETUP_ROOT/bin/colors.sh"
+
+echo -e "${IGreen}>>>${RST} ${BIWhite}Welcome to Pipe Club${RST}"
+echo -e "${IGreen}>>>${RST} launching Conda ..."
+
+# Launch Conda
+source "$PYPE_SETUP_ROOT/bin/launch_conda.sh"
+
+
+echo -e "${IGreen}>>>${RST} launching event server ..."
+python "$PYPE_SETUP_ROOT/app/pype-start.py" "--eventserver"
