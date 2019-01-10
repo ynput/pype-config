cfae14288c5af3ae369dc94f15a6b68e493d2127
Jakub Jezek <jezschaj@gmail.com>
2019-01-10 13:22:43 +0100

    tweeking way wrapper starts premiera

--- bin/windows/premiere_pro_2019.bat
+++ bin/windows/premiere_pro_2019.bat
@@ -4,7 +4,8 @@ set __app__="Adobe Premiere Pro"
 set __exe__="C:\Program Files\Adobe\Adobe Premiere Pro CC 2019\Adobe Premiere Pro.exe"
 if not exist %__exe__% goto :missing_app
 
-start "pype-premiere-wrapper" python -u %PREMIERA_PATH%\pr_wrapper.py
+REM start "pype-premiere-wrapper" 
+python -u %PREMIERA_PATH%\init.py
 start %__app__% %__exe__% %*
 
 goto :eof
