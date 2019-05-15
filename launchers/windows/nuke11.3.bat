d6dc3d7ffbf248cdee39b071bd4b107428ba567a
Jakub Jezek <jezschaj@gmail.com>
2019-05-15 15:15:25 +0200

    feat(nuke): adding version 11.3v2

--- launchers/windows/nuke11.3.bat
+++ launchers/windows/nuke11.3.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set __app__="Nuke11.3v1"
-set __exe__="C:\Program Files\Nuke11.3v1\Nuke11.3.exe"
-# if not exist %__exe__% goto :missing_app
+set __app__="Nuke11.3v2"
+set __exe__="C:\Program Files\Nuke11.3v2\Nuke11.3.exe"
+if not exist %__exe__% goto :missing_app
 
 start %__app__% %__exe__% %*
 
