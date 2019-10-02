d9d6ba108125524cb8eef5332c67d8ac6cbd9e68
Jakub Jezek <jakub@pype.tools>
2019-10-02 15:19:35 +0200

    Revert "adding Nuke12 launchers for linux"
    This reverts commit edee1a527f99f2d5373166cfe2d3deb3bd31766d.

--- launchers/windows/nukex11.2.bat
+++ launchers/windows/nukex11.2.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set __app__="NukeX11.2v3"
-set __exe__="C:\Program Files\Nuke11.2v3\Nuke11.2.exe" --nukex
+set __exe__="C:\Program Files\Nuke11.2v3\Nuke11.2.exe" -nukex
 if not exist %__exe__% goto :missing_app
 
 start %__app__% %__exe__% %*
