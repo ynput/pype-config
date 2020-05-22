db806a0e356cb8e751d35fc72c82c94338918489
Jakub Jezek <jakub@orbi.tools>
2020-05-22 18:31:27 +0200

    feat(resolve): launching with pre imported resolve python coslole

--- launchers/windows/resolve_16.bat
+++ launchers/windows/resolve_16.bat
@@ -1,10 +1,15 @@
 @echo off
 
 set __app__="Resolve"
+set __appy__="Resolve Python Console"
 set __exe__="C:/Program Files/Blackmagic Design/DaVinci Resolve/Resolve.exe"
+set __py__="%PYTHON36_RES%/python.exe"
+
 if not exist %__exe__% goto :missing_app
 
 start %__app__% %__exe__% %*
+start %__appy__% %__py__% -i %PRE_PYTHON_SCRIPT%
+REM start %__appy__% %__py__% -m pip install ipython
 
 goto :eof
 
