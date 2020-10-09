8232b119518dc375bcd89a22961acdb447cf355e
Jakub Jezek <jakub@orbi.tools>
2020-10-09 15:44:11 +0200

    launcher TVPaint 11

---
+++ launchers/windows/tvpaint11.bat
@@ -0,0 +1,19 @@
+set __app__="TVPaint Animation 11"
+set __app_dir__="C:\Program Files\TVPaint Developpement\TVPaint Animation 11 (64bits)\"
+set __exe__="C:\Program Files\TVPaint Developpement\TVPaint Animation 11 (64bits)\TVPaint Animation 11 (64bits).exe"
+
+if not exist %__exe__% goto :missing_app
+
+pushd %__app_dir__%
+
+if "%PYPE_TVPAINT_PROJECT_FILE%"=="" (
+  start %__app__% %__exe__% %*
+) else (
+  start %__app__% %__exe__% "%PYPE_TVPAINT_PROJECT_FILE%" %*
+)
+
+goto :eof
+
+:missing_app
+    echo ERROR: %__app__% not found in %__exe__%
+    exit /B 1
