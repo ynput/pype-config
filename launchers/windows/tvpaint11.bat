d6778be0e2e5efa04b7ba9c1f5e9534ebbe5fb03
iLLiCiTiT <jakub.trllo@gmail.com>
2020-10-27 20:45:57 +0100

    tvpaint launcher execute python script

--- launchers/windows/tvpaint11.bat
+++ launchers/windows/tvpaint11.bat
@@ -1,16 +1,15 @@
 set __app__="TVPaint Animation 11"
-set __exe__="C:\Program Files\TVPaint Developpement\TVPaint Animation 11 (64bits)\TVPaint Animation 11 (64bits).exe"
+
 set __32bit__="C:\Program Files (x86)\TVPaint Developpement\TVPaint Animation 11 (32bits)\TVPaint Animation 11 (32bits).exe"
 set __64bit__="C:\Program Files\TVPaint Developpement\TVPaint Animation 11 (64bits)\TVPaint Animation 11 (64bits).exe"
 
+set __exe__=%__64bit__%
+
 if not exist %__exe__% goto :missing_app
 
+set "PYPE_SETUP_PATH=%PYPE_SETUP_PATH:\=/%"
+start %__app__% cmd.exe /k %PYPE_PYTHON_EXE% "%PYPE_SETUP_PATH%/repos/avalon-core/avalon/tvpaint/launch_script.py" %__exe__% "%PYPE_TVPAINT_PROJECT_FILE%"
 
-if "%PYPE_TVPAINT_PROJECT_FILE%"=="" (
-  start %__app__% %__exe__% %*
-) else (
-  start %__app__% %__exe__% "%PYPE_TVPAINT_PROJECT_FILE%" %*
-)
 
 goto :eof
 
